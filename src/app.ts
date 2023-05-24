3114a83c2ebea3efc9c1696aa2845934a3f89fdd
joao pedro <jjoao.monteiro15@gmail.com>
2023-05-23 22:54:54 -0300

    feat:create function initialized socket server

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import express from 'express'
 import http from 'http'
-import { Server } from 'socket.io'
+import { Server, Socket } from 'socket.io'
 import cors from 'cors'
 const app = express()
 app.use(cors())
 const server = http.createServer(app)
 
-const io = new Server(server)
+export function socketServer(httpServer: http.Server) {
+  const io = new Server(httpServer)
+  io.on('connection', (socket: Socket) => {
+    socket.on('hello', (msg: string) => {
+      io.emit('hello', msg)
+    })
+  })
+  return io
+}
 
+socketServer(server)
 
 export default server
