e6091c08bdabd556654694694ee534b996fffb83
joao pedro <jjoao.monteiro15@gmail.com>
2023-05-24 22:19:27 -0300

    test: testing connection one more channel

--- test/socket.spec.ts
+++ test/socket.spec.ts
@@ -5,9 +5,6 @@ import { socketServer } from '../src/app'
 import { AddressInfo } from 'net'
 
 describe('Socket Testing', () => {
-  it('ss', () => {
-    expect(1+1).toEqual(2)
-  })
   let clientSocket: ClientSocket
   let httpServer: HttpServer
   let io: Server
@@ -26,14 +23,19 @@ describe('Socket Testing', () => {
   })
 
   it('should return what it sends', (done) => {
-    clientSocket = Client(`http://localhost:${(httpServer.address() as AddressInfo).port}`)
-    let msg:string = 'mensagem'
-    clientSocket.emit('hello', msg)
-    clientSocket.on("hello", (msg) => {
-      expect(msg).toBe(msg)
+    clientSocket = Client(`http://localhost:${(httpServer.address() as AddressInfo).port}/channel01`)
+    // let message:string = 'mensagem-channel01'
+    clientSocket.on('teste', msg => {
+      expect(msg).toBe('connectou')
       clientSocket.close()
       done()
     })
+    // clientSocket.emit('connect', message)
+    // clientSocket.on('resp_connect', (msg) => {
+    //   expect(msg).toBe(message)
+    //   clientSocket.close()
+    //   done()
+    // })
   })
 })
 
